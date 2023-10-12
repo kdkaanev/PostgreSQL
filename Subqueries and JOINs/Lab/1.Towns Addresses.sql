49b28798a0a02da79432fa390d15f6d82a648b89
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-10-12 10:56:23 +0300

    task1

---
+++ Subqueries and JOINs/Lab/1.Towns Addresses.sql
@@ -0,0 +1,19 @@
+SELECT 
+	t.town_id,
+	t."name",
+	a.address_text
+FROM
+	towns AS t 
+		JOIN 
+			addresses AS a
+				ON a.town_id = t.town_id
+
+WHERE 
+	t.name IN ('San Francisco', 'Sofia','Carnation')
+ORDER BY
+	town_id,
+	address_id
+;
+		
+
+		
